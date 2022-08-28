379c845fceb720ccec6f387bb5fbd2dd8359feb4
RameshBharatha7 <111409407+RameshBharatha7@users.noreply.github.com>
2022-08-28 14:52:44 +0530

    Update ProductLoader.java

--- src/main/java/guru/springframework/bootstrap/ProductLoader.java
+++ src/main/java/guru/springframework/bootstrap/ProductLoader.java
@@ -28,19 +28,19 @@ public class ProductLoader implements ApplicationListener<ContextRefreshedEvent>
     public void onApplicationEvent(ContextRefreshedEvent event) {
 
         Product shirt = new Product();
-        shirt.setDescription("Spring Framework Guru Shirt");
-        shirt.setPrice(new BigDecimal("18.95"));
-        shirt.setImageUrl("https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_shirt-rf412049699c14ba5b68bb1c09182bfa2_8nax2_512.jpg");
-        shirt.setProductId("235268845711068308");
+        shirt.setDescription("Jenkins Text Book");
+        shirt.setPrice(new BigDecimal("1099.99"));
+        shirt.setImageUrl("https://images-na.ssl-images-amazon.com/images/I/51WIZ7D7DYL.jpg");
+        shirt.setProductId("34562");
         productRepository.save(shirt);
 
         log.info("Saved Shirt - id: " + shirt.getId());
 
         Product mug = new Product();
-        mug.setDescription("Spring Framework Guru Mug");
-        mug.setImageUrl("https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_coffee_mug-r11e7694903c348e1a667dfd2f1474d95_x7j54_8byvr_512.jpg");
-        mug.setProductId("168639393495335947");
-        mug.setPrice(new BigDecimal("11.95"));
+        mug.setDescription("Kubernetes Text Book");
+        mug.setImageUrl("https://m.media-amazon.com/images/I/41azDS-7v7L.jpg");
+        mug.setProductId("16863");
+        mug.setPrice(new BigDecimal("1999.95"));
         productRepository.save(mug);
 
         log.info("Saved Mug - id:" + mug.getId());
